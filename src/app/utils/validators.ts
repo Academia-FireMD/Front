a7162d2e2bfecdfdc57f81e850e7168635c46d91
Tihomir Stoychev Stoychev <tihomir_alcudia3@hotmail.com>
2025-09-01 11:56:07 +0200

    Refactor registro.component.html and validators.ts for improved readability and functionality
    - Cleaned up HTML structure in registro.component.html by removing unnecessary whitespace and ensuring consistent formatting.
    - Updated app-comunidad-dropdown to include a full-width class for better layout.
    - Simplified password strength validation logic in validators.ts by removing the special character requirement, enhancing user experience while maintaining security standards.

--- src/app/utils/validators.ts
+++ src/app/utils/validators.ts
@@ -20,14 +20,12 @@ export function passwordStrengthValidator(): ValidatorFn {
     const hasUpperCase = /[A-Z]/.test(value);
     const hasLowerCase = /[a-z]/.test(value);
     const hasNumeric = /[0-9]/.test(value);
-    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(value);
     const hasMinLength = value.length >= 8;
 
     const passwordValid =
       hasUpperCase &&
       hasLowerCase &&
       hasNumeric &&
-      hasSpecialChar &&
       hasMinLength;
 
     return !passwordValid ? { 'passwordStrength': true } : null;
